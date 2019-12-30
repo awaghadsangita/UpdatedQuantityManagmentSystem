eb811d41427e1dd8309939d300c7db1edbb718a7
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-30 19:50:21 +0530

    REFACTORED:code & test to specifically address Feet

--- main/Length.js
+++ main/Length.js
@@ -1,17 +1,20 @@
+const LengthUnit={
+    FEET:12
+}
 class Length{
-    constructor(value){
-        this.value=value;
+    constructor(value,lengthUnit){
+        this.unitValue=value;
+        this.lengthUnitType=lengthUnit;
     }
-    
+ 
     equalality(thatObj){
         if(thatObj===null){
             return false;
         }
-        if(this===thatObj||(this.value==thatObj.value && typeof thatObj===typeof new Length())){
+        if(this===thatObj||(this.unitValue==thatObj.unitValue && typeof thatObj===typeof new Length() && this.lengthUnitType==thatObj.lengthUnitType)){
             return true;
         }
         return false;
     }
-
 }
-module.exports=Length;
+module.exports={Length,LengthUnit};
--- test/LengthTest.js
+++ test/LengthTest.js
@@ -2,23 +2,23 @@ let assert=require('assert');
 let length=require('../main/Length')
 describe('tests for length conversion',function(){
     it('given 0 feet and 0 feet should return true',function(){
-        zeroFeet=new length(1);
-        anotherZeroFeet=new length(1);
+        zeroFeet=new length.Length(1,length.LengthUnit.FEET);
+        anotherZeroFeet=new length.Length(1,length.LengthUnit.FEET);
         assert.equal(anotherZeroFeet.equalality(zeroFeet),true);
     })
 
     it('given 0 feet object and null check for equality should return false',function(){
-        zeroFeet=new length(0);
+        zeroFeet=new length.Length(0,length.LengthUnit.FEET);
         assert.equal(false,zeroFeet.equalality(null));
     })
 
     it('given 0 feet object check itself for equality should return false',function(){
-        zeroFeet=new length(0);
+        zeroFeet=new length.Length(0,length.LengthUnit.FEET);
         assert.equal(true,zeroFeet.equalality(zeroFeet));
     })
 
     it('given different typeof object for equality should return false',function(){
-        zeroFeet=new length(0);
+        zeroFeet=new length.Length(0,length.LengthUnit.FEET);
         let object=new Object();
         assert.equal(false,zeroFeet.equalality(object));
     })
