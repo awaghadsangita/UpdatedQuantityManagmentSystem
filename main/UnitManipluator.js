8a7d6c667a12df8fe5f3b2a9a7bf7545941c8aa6
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-31 17:41:59 +0530

    ADDED:code for addition of two units

--- main/UnitManipluator.js
+++ main/UnitManipluator.js
@@ -1,27 +1,31 @@
-class UnitManipulator{
-    constructor(unitValue,unitType){
-        this.unitValue=unitValue;
-        this.unitType=unitType;
+class UnitManipulator {
+    constructor(unitValue, unitType) {
+        this.unitValue = unitValue;
+        this.unitType = unitType;
     }
 
-    compare(unitOne,unitTwo){
-        if(unitOne.unitType===null||unitTwo.unitType===null)
+    compare(unitOne, unitTwo) {
+        if (unitOne.unitType === null || unitTwo.unitType === null || unitOne.unitType === undefined || unitTwo.unitType === undefined)
             throw new Error('Invalid Unit Type');
-                
-        if(unitOne.unitType.name==unitTwo.unitType.name)
-            return unitOne.unitValue*unitOne.unitType.value==unitTwo.unitValue*unitTwo.unitType.value;
-            
+
+        if (unitOne.unitType.name == unitTwo.unitType.name)
+            return unitOne.unitValue * unitOne.unitType.value == unitTwo.unitValue * unitTwo.unitType.value;
+
         throw new Error('Invalid Unit Type');
     }
 
-    equal(unitOne,unitTwo){
-        if(unitOne===null || unitTwo===null){
+    add(unitOne, unitTwo) {
+        return (unitOne.unitValue * unitOne.unitType.value) + (unitTwo.unitValue * unitTwo.unitType.value);
+    }
+
+    equal(unitOne, unitTwo) {
+        if (unitOne === null || unitTwo === null) {
             return false;
         }
-        if(unitOne===unitTwo||(unitOne.unitValue==unitTwo.unitValue && typeof unitOne===typeof unitTwo && unitOne.unitType==unitTwo.unitType)){
+        if (unitOne === unitTwo || (unitOne.unitValue == unitTwo.unitValue && typeof unitOne === typeof unitTwo && unitOne.unitType == unitTwo.unitType)) {
             return true;
         }
         return false;
     }
 }
-module.exports=UnitManipulator;
+module.exports = UnitManipulator;
