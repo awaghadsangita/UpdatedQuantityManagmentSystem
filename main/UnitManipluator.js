dc5567db1a02edea50292585790e9666f930497a
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-31 13:21:35 +0530

    ADDED:code for unit type checking

--- main/UnitManipluator.js
+++ main/UnitManipluator.js
@@ -4,8 +4,20 @@ class UnitManipulator{
         this.unitType=unitType;
     }
 
-    unitConversion(){
-        return this.unitValue*this.unitType;
+    compare(unitOne,unitTwo){
+        if(typeof unitOne.unitType==typeof unitTwo.unitType)
+            return unitOne.unitValue*unitOne.unitType==unitTwo.unitValue*unitTwo.unitType;
+        
+    }
+
+    equal(unitOne,unitTwo){
+        if(unitOne===null || unitTwo===null){
+            return false;
+        }
+        if(unitOne===unitTwo||(unitOne.unitValue==unitTwo.unitValue && typeof unitOne===typeof unitTwo && unitOne.unitType==unitTwo.unitType)){
+            return true;
+        }
+        return false;
     }
 }
 module.exports=UnitManipulator;
