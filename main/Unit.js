9561e8e4daeae64f1c331150141d4a83e87a80fb
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-31 12:06:35 +0530

    ADDED:volume and length constant in single class

---
+++ main/Unit.js
@@ -0,0 +1,14 @@
+class Unit{
+    LengthUnit={
+        FEET:12,
+        INCH:1,
+        YARD:36,
+        CENTIMETER:0.393701
+    }
+    volumeUnit={
+        GALLON:3.78,
+        MILILITIRE:0.001,
+        LITRE:1
+    }
+}
+module.exports=new Unit();
