d490fcaa6bc17bd9d28f9a930cc52a3fef489baa
sangita awaghad <sangita.awaghad@gmail.com>
2020-01-02 12:15:30 +0530

    REFACTORED:made code more readable

--- main/UnitManipluator.js
+++ main/UnitManipluator.js
@@ -6,7 +6,12 @@ class UnitManipulator {
     }
 
     compare(unitOne, unitTwo) {
-        if (unitOne == null || unitTwo == null || unitOne.unitType === null || unitTwo.unitType === null || unitOne.unitType === undefined || unitTwo.unitType === undefined)
+        if (unitOne == null 
+            || unitTwo == null 
+            || unitOne.unitType === null 
+            || unitTwo.unitType === null 
+            || unitOne.unitType === undefined 
+            || unitTwo.unitType === undefined)
             throw new Error('Invalid Unit');
 
         if (unitOne.unitType == unit.temparatureUnit.FAHRENHEIT)
@@ -22,7 +27,15 @@ class UnitManipulator {
     }
 
     add(unitOne, unitTwo) {
-        if (unitOne == null || unitTwo == null || unitOne == undefined || unitTwo == undefined || unitOne.unitType === null || unitTwo.unitType === null || unitOne.unitType === undefined || unitTwo.unitType === undefined || unitOne.unitType.type != unitTwo.unitType.type)
+        if (unitOne == null 
+            || unitTwo == null 
+            || unitOne == undefined 
+            || unitTwo == undefined 
+            || unitOne.unitType === null 
+            || unitTwo.unitType === null 
+            || unitOne.unitType === undefined 
+            || unitTwo.unitType === undefined 
+            || unitOne.unitType.type != unitTwo.unitType.type)
             throw new Error('Invalid Unit');
 
         return (unitOne.unitValue * unitOne.unitType.value) + (unitTwo.unitValue * unitTwo.unitType.value);
@@ -32,7 +45,10 @@ class UnitManipulator {
         if (unitOne === null || unitTwo === null) {
             return false;
         }
-        if (unitOne === unitTwo || (unitOne.unitValue == unitTwo.unitValue && typeof unitOne === typeof unitTwo && unitOne.unitType == unitTwo.unitType)) {
+        if (unitOne === unitTwo 
+            || (unitOne.unitValue == unitTwo.unitValue 
+                && typeof unitOne === typeof unitTwo 
+                && unitOne.unitType == unitTwo.unitType)) {
             return true;
         }
         return false;
